141c5874bb84afffc95d20ecea8a6187a6766143
Alek Merani <alekmerani@gmail.com>
2025-09-08 21:25:09 -0700

    reconnect ex

--- index.tsx
+++ index.tsx
@@ -1,23 +1,59 @@
-import React, { useCallback } from 'react'
+import React, { useCallback, useId, useState } from 'react'
 import * as ReactDOM from 'react-dom/client'
 import { ReactEventStream, useEventStream } from './dist/ReactEventStream'
-import './terminal-styles.css'
+import './index.css'
 
-const HookEventStream: React.ComponentType = () => {
-    const stream:any = useEventStream(
-        'http://localhost:3001/sse', 
-        useCallback((event: { type: string, word: string }): string|undefined => {
-            switch (event.type) {
-                case 'chunk':
-                    return `${event.word} `;
-                case 'completed':
-                    return undefined;
-                default:
-                    return '';
-            }
-        }, []),
-    )!;
+const config = {
+    url: 'http://localhost:3001/sse',
+    onEvent: (event: { type: string, word: string }) => event.type === 'completed' ? undefined : `${event.word ?? ''} `
+}
 
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(<App />);
+
+function App() {
+    const [eventType, setEventType] = useState("");
+    const [id, setId] = useState(Number.MAX_SAFE_INTEGER);
+    const onEvent = (event) => {
+        setEventType(event.type);
+        return config.onEvent(event);
+    }
+    const reconnect = () => setId(id => id-1);
+    return (
+        <div className="terminal-page">
+            <header>
+                <button onClick={reconnect}>
+                    <b>Click to Reconnect</b>
+                    <br/>
+                    ({eventType === "chunk" ? "streaming": eventType})
+                </button>
+            </header>            
+            <Streams onEvent={onEvent} key={id} />
+        </div>   
+    )
+}
+
+function Streams({ onEvent }) {
+    return (
+        <>
+            <section className="terminal-section">
+                <h1 className="terminal-header">ReactEventStream Component</h1>
+                <ReactEventStream 
+                    url={config.url} 
+                    onEvent={onEvent}
+                    render={(stream) => <div className="terminal-window">{stream}</div>}
+                />
+            </section>
+            <section className="terminal-section">
+                <h1 className="terminal-header">useEventStream() Hook</h1>
+                <HookEventStream />
+            </section>        
+        </>        
+    )
+}
+
+function HookEventStream() {
+    const stream:any = useEventStream(config.url, config.onEvent);
     return (
         <div className="terminal-window">
             {stream?.length > 0 ? String(stream) : (
@@ -26,25 +62,3 @@ const HookEventStream: React.ComponentType = () => {
         </div>
     )
 }
-
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(
-    <div className="terminal-page">
-        <section className="terminal-section">
-            <h1 className="terminal-header">ReactEventStream Component</h1>
-            <ReactEventStream 
-                url="http://localhost:3001/sse" 
-                onEvent={(event) => event.type === 'completed' ? undefined : `${event.word ?? ''} `}
-                render={(stream) => (
-                    <div className="terminal-window">
-                        {stream}
-                    </div>
-                )}
-            />
-        </section>
-        <section className="terminal-section">
-            <h1 className="terminal-header">useEventStream() Hook</h1>
-            <HookEventStream />
-        </section>
-    </div>
-)
