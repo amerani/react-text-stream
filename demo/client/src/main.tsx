065aebab74ac7f6cbafe8eccd82053ae1dc17d5e
Alek Merani <alekmerani@gmail.com>
2026-04-11 17:21:13 -0700

    reorg client demo

---
+++ demo/client/src/main.tsx
@@ -0,0 +1,69 @@
+import { useState } from 'react'
+import * as ReactDOM from 'react-dom/client'
+import { ReactTextStream, useTextStream } from 'react-text-stream'
+import './index.css'
+
+const config = {
+  url: 'http://localhost:3001/sse',
+  onEvent: (event: { type: string; word: string }) =>
+    event.type === 'completed' ? undefined : `${event.word ?? ''} `,
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+root.render(<App />)
+
+function App() {
+  const [eventType, setEventType] = useState('')
+  const [id, setId] = useState(Number.MAX_SAFE_INTEGER)
+  const onEvent = (event: { type: string; word: string }) => {
+    setEventType(event.type)
+    return config.onEvent(event)
+  }
+  const reconnect = () => setId((id) => id - 1)
+  return (
+    <div className="terminal-page">
+      <header>
+        <button type="button" onClick={reconnect}>
+          <b>Click to Reconnect</b>
+          <br />
+          ({eventType === 'chunk' ? 'streaming' : eventType})
+        </button>
+      </header>
+      <Streams onEvent={onEvent} key={id} />
+    </div>
+  )
+}
+
+function Streams({
+  onEvent,
+}: {
+  onEvent: (event: { type: string; word: string }) => string | undefined
+}) {
+  return (
+    <>
+      <section className="terminal-section">
+        <h1 className="terminal-header">ReactTextStream Component</h1>
+        <ReactTextStream
+          url={config.url}
+          onEvent={onEvent}
+          render={(stream) => <div className="terminal-window">{stream}</div>}
+        />
+      </section>
+      <section className="terminal-section">
+        <h1 className="terminal-header">useTextStream() Hook</h1>
+        <HookTextStream />
+      </section>
+    </>
+  )
+}
+
+function HookTextStream() {
+  const stream = useTextStream(config.url, config.onEvent)
+  return (
+    <div className="terminal-window">
+      {stream && stream.length > 0 ? String(stream) : (
+        <span className="terminal-generating">Generating...</span>
+      )}
+    </div>
+  )
+}
